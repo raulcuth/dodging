5e0c981a9c3e8af839b1143ce704a3b04d7a9c9b
raulcuth <raul.cuth@gmail.com>
2019-05-21 23:13:12 +0300

    added first draft of scoring system by destroyed objects(dodged)

--- Assets/Scripts/DeleteObstacles.cs
+++ Assets/Scripts/DeleteObstacles.cs
@@ -1,11 +1,23 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class DeleteObstacles : MonoBehaviour
-{
+public class DeleteObstacles : MonoBehaviour {
+    public Text scoreText;
+    public float obstacleScore;
+
     private void OnCollisionEnter(Collision collision) {
         if (collision.gameObject.tag.Equals("Obstacle") || collision.gameObject.tag.Equals("ImpossibleObstacle")) {
             collision.gameObject.SetActive(false);
             Destroy(collision.gameObject);
+            scoreText.text = (int.Parse(scoreText.text) + obstacleScore).ToString();
         }
+    }
+
+    private int ParseFastInteger(string value) {
+        int result = 0;
+        foreach (char c in value) {
+            result = 10 * result + (c - 48);
+        }
+        return result;
     }
 }
