af91056988ba1505a5c60a853a2ae5b3fd1bc669
raulcuth <raul.cuth@gmail.com>
2019-05-21 22:08:09 +0300

    changed delete obstacle script

--- Assets/Scripts/DeleteObstacles.cs
+++ Assets/Scripts/DeleteObstacles.cs
@@ -3,7 +3,7 @@
 public class DeleteObstacles : MonoBehaviour
 {
     private void OnCollisionEnter(Collision collision) {
-        if (collision.gameObject.tag.Equals("Obstacle")) {
+        if (collision.gameObject.tag.Equals("Obstacle") || collision.gameObject.tag.Equals("ImpossibleObstacle")) {
             collision.gameObject.SetActive(false);
             Destroy(collision.gameObject);
         }
